799bcb3dcbbeb21c33f8d1834400b875cbde1d0c
antoniaa286 <mantoniabarbu@gmail.com>
2024-12-12 13:27:36 +0200

    implemented search and filter functionalities

--- backend/routes/movie_routes.py
+++ backend/routes/movie_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from utils.db import get_db
 
 movies_blueprint = Blueprint('movies', __name__)
@@ -9,3 +9,27 @@ def list_movies():
     movies = list(db.movies.find({}, {'_id': 0}))
     return jsonify(movies), 200
 
+@movies_blueprint.route('/search-movie', methods=['GET'])
+def search_movies():
+    db = get_db()
+    title = request.args.get('title')
+    
+    if not title:
+        return jsonify({'message': 'Invalid request'}), 400
+    
+    query = {'title': {'$regex': title, '$options': 'i'}}
+    movies = list(db.movies.find(query, {'_id': 0}))
+
+    return jsonify(movies), 200
+
+@movies_blueprint.route('/filter-genre', methods=['GET'])
+def filter_genre():
+    db = get_db()
+    genre = request.args.get('genre')
+
+    if not genre:
+        return jsonify({'message': 'Invalid request'}), 400
+    query = {'genres':genre}
+    movies = list(db.movies.find(query, {'_id': 0}))
+
+    return jsonify(movies), 200
