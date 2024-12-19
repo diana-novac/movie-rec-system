8d3ccb93750b2252d7682989173bdc2fb48a5e23
antoniaa286 <mantoniabarbu@gmail.com>
2024-12-19 16:02:29 +0200

    content_based_recommendations

--- backend/utils/recommendations.py
+++ backend/utils/recommendations.py
@@ -14,3 +14,22 @@ def load_data():
     ratings_df = pd.DataFrame(ratings)
 
     return movies_df, ratings_df
+
+
+def content_based_recommendations(movie_id, movies_df):
+    movies_df['genres combined'] = movies_df['genres'].apply(lambda x:' '.join(x))
+
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(movies_df['genres_combined'])
+
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+
+    indices = pd.Series(movies_df.index, index = movies_df['movieId'])
+
+    idx = indices[movie_id]
+
+    sim_score= list(enumerate(cosine_sim[idx]))
+
+    movie_indices = [i[0] for i in sim_score[1:11]]
+
+    return movies_df.iloc[movie_indices][['movieId', 'title', 'genres']]
